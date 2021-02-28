cb959f46893a73ce8f9f279d5b54b888348814aa
BBranko93 <bane_b80@outlook.com>
2021-02-27 20:42:57 -0700

    Add project files.

---
+++ FibonacciSequenceUpToGivenLimitForLoop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FibonacciSequenceUpToGivenLimitForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0, n2 = 1, n3, i, num;
+
+            Console.WriteLine("Please enter number for terms to be displayed : ");
+            num = int.Parse(Console.ReadLine());
+            Console.Write(n1 + " " + n2 );
+            for(i = 3; i <= num; i++)
+            {
+                n3 = n1 + n2;
+                Console.Write( " " + n3 + " ");
+                n1 = n2;
+                n2 = n3;
+
+            }
+            Console.Write("\n");
+        }
+    }
+}
